930135a84acaa8bd50924ee8a98459f2e8980caa
Awuor-Nancy <anyangnancy@gmail.com>
2022-02-26 21:35:34 +0300

    first commit

---
+++ src/Main.kt
@@ -0,0 +1,35 @@
+fun main() {
+    var text = "akirachix"
+   //println(text[0])//first element
+   //println(name[1])
+    //println(text[2])//third element
+    //println(text[3])//fourth element
+    //println(text[4])
+    //println(text[5])
+    //println(text[6])
+    //println(text[7])
+    //println(text[8])
+    var result = text[0].toString() + text[2] + text[3]
+    println(result)
+
+
+    var name = "Jane"
+    val age=10
+    var greeting = "Hi, i am $name I am $age yrs old"
+    println(greeting)
+
+    var item = "cholocate"
+    println(item.length)
+
+    var fname ="john"
+    if (fname == "john"
+) {
+        println("Thats' me!")
+    }
+        else{
+            print("I don't know who that is")
+        }
+}
+
+
+
